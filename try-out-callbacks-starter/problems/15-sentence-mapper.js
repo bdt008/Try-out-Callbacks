665d4dd36f2de63b8768e735f429741207edef3b
Tadd JV <taddjv111@gmail.com>
2022-02-09 18:43:00 -0500

    15th one

--- try-out-callbacks-starter/problems/15-sentence-mapper.js
+++ try-out-callbacks-starter/problems/15-sentence-mapper.js
@@ -25,14 +25,32 @@ let result2 = sentenceMapper("this is pretty cool right", removeVowels);
 console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
-let sentenceMapper = function() {
-
+let sentenceMapper = function (string, cb) {
+  let newSen = string.split(" ");
+  newSen.map(function (word, i) {
+    newSen[i] = cb(word);
+  });
+  return newSen.join(" ");
 };
 
+let result1 = sentenceMapper("what is the answer?", function (word) {
+  return word.toUpperCase() + "!";
+});
+console.log(result1); // 'WHAT! IS! THE! ANSWER?!'
 
+let removeVowels = function (word) {
+  let newWord = "";
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+    if (!"aeiou".includes(char)) {
+      newWord += char;
+    }
+  }
+  return newWord;
+};
 
-
-
+let result2 = sentenceMapper("this is pretty cool right", removeVowels);
+console.log(result2); // 'ths s prtty cl rght'
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = sentenceMapper;

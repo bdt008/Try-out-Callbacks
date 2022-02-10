9480ed3df1f59ef6e9560246bc0f93d56854f4dc
Tadd JV <taddjv111@gmail.com>
2022-02-09 20:05:13 -0500

    19th one

--- try-out-callbacks-starter/problems/19-greater-callback-value.js
+++ try-out-callbacks-starter/problems/19-greater-callback-value.js
@@ -18,13 +18,27 @@ console.log(greaterCallbackValue(1, doubler, squarer));     // 2
 console.log(greaterCallbackValue(9, Math.sqrt, doubler));   // 18
 *******************************************************************************/
 
-let greaterCallbackValue = function() {
-
+let greaterCallbackValue = function (val, cb1, cb2) {
+  let greaterVal = undefined;
+  if (cb1(val) > cb2(val)) {
+    greaterVal = cb1(val);
+  } else {
+    greaterVal = cb2(val);
+  }
+  return greaterVal;
 };
 
+let doubler = function (n) {
+  return 2 * n;
+};
 
+let squarer = function (n) {
+  return n * n;
+};
 
-
+console.log(greaterCallbackValue(5, doubler, squarer)); // 25
+console.log(greaterCallbackValue(1, doubler, squarer)); // 2
+console.log(greaterCallbackValue(9, Math.sqrt, doubler)); // 18
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = greaterCallbackValue;

1003a0feb2d0bf0e5ffa7f0a3673e30bcb472cc8
Tadd JV <taddjv111@gmail.com>
2022-02-09 20:23:51 -0500

    21st

--- try-out-callbacks-starter/problems/21-at-most.js
+++ try-out-callbacks-starter/problems/21-at-most.js
@@ -22,14 +22,29 @@ console.log(atMost(['boat', 'arc', 'cat', 'car', 'academy'], 1, startsWithA));  
 
 *******************************************************************************/
 
-let atMost = function() {
-
+let atMost = function (arr, max, cb) {
+  let counter = 0;
+  arr.forEach(function (num) {
+    if (cb(num) === true) {
+      counter += 1;
+    }
+  });
+  return counter <= max;
 };
 
+let isPositive = function (n) {
+  return n > 0;
+};
+let startsWithA = function (s) {
+  return s[0].toUpperCase() === "A";
+};
 
-
-
-
+console.log(atMost([6, -2, 4, -1], 3, isPositive)); // true
+console.log(atMost([6, -2, 4, 1], 3, isPositive)); // true
+console.log(atMost([6, 2, 4, 1], 3, isPositive)); // false
+console.log(atMost(["boat", "cat", "car"], 1, startsWithA)); // true
+console.log(atMost(["boat", "cat", "car", "academy"], 1, startsWithA)); // true
+console.log(atMost(["boat", "arc", "cat", "car", "academy"], 1, startsWithA)); // false
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = atMost;
